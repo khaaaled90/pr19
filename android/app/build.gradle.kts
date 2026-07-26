ca02d0b8b5619448877c106f345d3795dc0db5a8
khaled hatem <143466388+khaaaled90@users.noreply.github.com>
2026-07-26 06:24:17 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,6 +36,12 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true  // 👈 تفعيل التقليص مع استثناء الكلاس عبر ProGuard
+            isShrinkResources = false
+    
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
         }
     }
 }

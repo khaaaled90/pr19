511a0c9f1aa45e19e2f1fb9cad5a75b945132f8f
khaaaled90 <khaled.hatem90@gmail.com>
2026-07-26 03:18:35 +0300

    Update android/build.gradle.kts with telephony fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,8 +12,21 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+// ⬇️ الجزء الخاص بحل مشكلة مكتبة telephony والـ namespace ⬇️
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library")) {
+            val android = extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)
+            if (android != null && android.namespace == null) {
+                android.namespace = project.group.toString().ifEmpty { "com.example.${project.name}" }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {

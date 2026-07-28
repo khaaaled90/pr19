3f2d65f94d0fb9b4480644e3fe073f5268ee1559
khaled hatem <143466388+khaaaled90@users.noreply.github.com>
2026-07-28 06:10:30 +0300

    Update SmsReceiver.kt

--- android/app/src/main/kotlin/com/example/pr19/SmsReceiver.kt
+++ android/app/src/main/kotlin/com/example/pr19/SmsReceiver.kt
@@ -10,9 +10,15 @@ import androidx.work.WorkManager
 
 class SmsReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        Log.d("SMS_RECEIVER", "onReceive Called")
         if (intent.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+            Log.d("SMS_RECEIVER", "SMS_RECEIVED_ACTION")
             val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
             for (sms in messages) {
+                Log.d(
+                    "SMS_RECEIVER",
+                    "Sender=${sms.originatingAddress} Body=${sms.messageBody}"
+                )
                 val sender = sms.originatingAddress ?: continue
                 val body = sms.messageBody ?: continue
 

55c8d73a1cac37167df5955333e96673d32bbc80
khaled hatem <143466388+khaaaled90@users.noreply.github.com>
2026-07-28 06:49:18 +0300

    Update NativeNotificationListener.kt

--- android/app/src/main/kotlin/com/example/pr19/NativeNotificationListener.kt
+++ android/app/src/main/kotlin/com/example/pr19/NativeNotificationListener.kt
@@ -45,19 +45,17 @@ class NativeNotificationListener : NotificationListenerService() {
         Log.d("NOTIFICATION", "Title = $title")
         Log.d("NOTIFICATION", "Text = $text")
         Log.d("NOTIFICATION", "Content = $fullContent")
-
         // فحص قاعدة البيانات
-        val dbHelper = AppSqliteHelper.getInstance(applicationContext)
-
+        /*val dbHelper = AppSqliteHelper.getInstance(applicationContext)
         val allowed = dbHelper.isSenderAllowed(packageName)
-
         Log.d("NOTIFICATION", "Allowed = $allowed")
-
         // إذا لم يكن التطبيق ضمن التطبيقات المسموح بها
         if (!allowed) {
             Log.d("NOTIFICATION", "Notification ignored")
             return
-        }
+        }*/
+        // ===== تم تعطيل فلترة التطبيقات مؤقتاً =====
+        Log.d("NOTIFICATION", "Filtering disabled - accepting all notifications")
 
         Log.d("NOTIFICATION", "Creating WorkManager request")
 
